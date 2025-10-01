b06da6f4b7b4538fbcd952ea36ce443f0b90164f
bequ1nG0 <kulikovskikh@bk.ru>
2025-10-01 07:06:28 +0300

    resolv problem with segfault on triangulation task

--- triangulation/src/TriangulationTask.cpp
+++ triangulation/src/TriangulationTask.cpp
@@ -12,35 +12,6 @@
 using namespace std;
 using namespace cfg;
 
-int time2int(const string& time) 
-{
-    Logger logger(".log");
-    if (time.length() < 12)// Проверка длины
-    {
-        logger.addWriting("Time string too short", 'E');
-        return 0;
-    }
-    else if (time[2] != ':' || time[5] != ':' || time[8] != ':')
-    {   
-        logger.addWriting("Invalid time format", 'E');
-        return 0;
-    }
-    try 
-    {
-        int hours = (time[0]-'0')*10*60*60*1000 + (time[1]-'0')*60*60*1000;
-        int min = (time[3]-'0')*10*60*1000 + (time[4]-'0')*60*1000;
-        int sec = (time[6]-'0')*10*1000 + (time[7]-'0')*1000;
-        int ms = (time[9]-'0')*100 + (time[10]-'0')*10 + (time[11]-'0');
-        
-        return hours + min + sec + ms;
-    } 
-    catch (...) 
-    {
-        logger.addWriting("Invalid characters in time", 'E');
-        return 0;
-    }
-}
-
 bool compareSensor(const Sensor& a, const Sensor& b)
 {
     return a.mac < b.mac;
@@ -53,32 +24,9 @@ bool compareMessage(const SensorMessage& a, const SensorMessage& b)
 
 TriangulationTask::TriangulationTask(vector <Sensor> sensors, vector <SensorMessage> sensors_messages)
 { //переписать, добавить массив messages с которыми работаем, добавить массив датчиков с которыми работаем.
-    int delta_time = 100; u_int64_t temp = 0; 
-    vector <SensorMessage> messages;
-    vector <Sensor> this_sensors;
-    /*
-    sort(sensors.begin(), sensors.end(), compareSensor);
-    sort(sensors_messages.begin(), sensors_messages.end(), compareMessage); */
-    temp = sensors_messages[sensors_messages.size()-1].timestump;
-    for(auto e : sensors_messages)
-    {
-        if(abs( static_cast<int64_t>(e.timestump - temp)) <= delta_time)
-        {
-            messages.push_back(e);
-        }
-        if(messages.size() == 3) break;
-    }
-    for(auto e : messages)
-    {
-        for(auto k : sensors)
-        {
-            if(e.mac == k.mac) this_sensors.push_back(k);
-        }
-    }
-    if(this_sensors.size() == 3 && messages.size() == 3) Triangulator triangulator(this_sensors, messages);
-    else if(this_sensors.size() > 3 || messages.size() > 3) Triangulator triangulator({this_sensors[0], this_sensors[1], this_sensors[2]},{messages[0], messages[1], messages[2]});
+    Triangulator triangulator(sensors, sensors_messages);
     Logger logger(".log");
-    logger.addWriting("make task", 'I');
+    logger.addWriting("Успешно создана задача", 'I');
 
 }
 
